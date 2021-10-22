17f13613c5d4729a8b2f33a8041f38e348b07aa1
oseeshogun <omasuaku@gmail.com>
2021-10-22 10:16:20 +0100

    ok

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,20 +1,22 @@
-import { Express } from 'express';
-import { Model, ModelCtor, Sequelize } from 'sequelize';
-import hash from 'password-hash';
+import { Express } from "express";
+import { Model, ModelCtor, Sequelize } from "sequelize";
+import hash from "password-hash";
 
 export default (
   app: Express,
   sequelize: Sequelize,
   User: ModelCtor<Model<any, any>>
 ) => {
-  app.post('/register/', (req, res) => {
+  app.post("/register/", (req, res) => {
     const { password, email } = req.body;
 
     const generatedPassword = hash.generate(password);
     const token: string = hash.generate(Date().toString());
 
     User.findOne({ where: { email } }).then((dbUser) => {
-      if (dbUser !== null) { return res.sendStatus(412); }
+      if (dbUser !== null) {
+        return res.sendStatus(412);
+      }
       User.create({
         email,
         password: generatedPassword,
@@ -31,7 +33,7 @@ export default (
     });
   });
 
-  app.get('/users/', (req, res) => {
+  app.get("/users/", (req, res) => {
     User.findAll()
       .then((users) => {
         return res.json(users);
@@ -42,13 +44,15 @@ export default (
       });
   });
 
-  app.post('/login/', (req, res) => {
+  app.post("/login/", (req, res) => {
     const { password, email } = req.body;
 
     User.findOne({ where: { email } })
       .then((user) => {
-        if (user == null) { return res.sendStatus(404); }
-        const dbPassword: string = user.getDataValue('password');
+        if (user == null) {
+          return res.sendStatus(404);
+        }
+        const dbPassword: string = user.getDataValue("password");
         const verified: boolean = hash.verify(password, dbPassword);
         if (verified) {
           const token: string = hash.generate(Date().toString());
@@ -69,22 +73,32 @@ export default (
       });
   });
 
-  app.post('/update_user/', (req, res) => {
+  app.post("/update_user/", (req, res) => {
     const { email, name, prenom, address, phone } = req.body;
 
     User.findOne({ where: { email } })
       .then((user) => {
-        if (user == null) { return res.sendStatus(404); }
+        if (user == null) {
+          return res.sendStatus(404);
+        }
         User.update(
           { name, prenom, phone, address },
           {
             where: { email },
           }
-        );
-        return res.json({
-          ...user.toJSON(),
-          ...{ name, prenom, phone, address },
-        });
+        )
+          .then((value) => {
+            User.findOne({ where: { email } })
+              .then((user) => {
+                return res.json(user.toJSON());
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
       .catch((err) => {
         console.log(err);
